8c89186886b72469dc42e8c8066498b9f0abbfb9
Facundo Figueroa Development <facundo.figueroa.dev@mail.com>
2026-04-21 15:34:26 -0300

    feat(sequelize): sequelize connection - create sequelize connection - create sequelize config file

---
+++ back/src/db/connection/sequelize.js
@@ -0,0 +1,14 @@
+import { Sequelize } from 'sequelize';
+import envs from '../../envs/index.js';
+console.log(envs);
+
+export const sequelize = new Sequelize(
+  envs.db_name,
+  envs.db_user,
+  envs.db_pass,
+  {
+    host: envs.db_host,
+    port:envs.db_port,
+    dialect: 'postgres',
+  },
+);
