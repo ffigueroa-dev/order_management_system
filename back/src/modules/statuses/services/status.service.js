4747c86a74e32b54d363940c1a7050c3e4dc6722
Facundo Figueroa Development <facundo.figueroa.dev@mail.com>
2026-04-27 18:56:27 -0300

    feat(status): update status - create dinById method - create updateByIdMethod - create schema to validate id - create updateStatusSchema - added route to update status at controller

--- back/src/modules/statuses/services/status.service.js
+++ back/src/modules/statuses/services/status.service.js
@@ -1,3 +1,4 @@
+import Boom  from '@hapi/boom';
 import { Status } from '../models/status.model.js';
 
 export class StatusService {
@@ -7,6 +8,27 @@ export class StatusService {
 
   createStatus = async (data) => {
     const status = await this.statusModel.create(data);
+    return status;
+  };
+  
+  
+  
+  
+  
+  
+  
+
+  findById = async (id) => {
+    const status = await this.statusModel.findOne({ where: { id: id } });
+    if (!status) {
+      throw Boom.notFound('status not found');
+    }
+    return status;
+  };
+
+  updateById = async (id, payload) => {
+    const status = await this.findById(id);
+    await status.update(payload);
     return status;
   };
 }
