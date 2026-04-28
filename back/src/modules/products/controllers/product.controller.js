87e9158564b47b2701935ca7f756e978d8cbab52
Facundo Figueroa Development <facundo.figueroa.dev@mail.com>
2026-04-28 19:14:55 -0300

    feat(product): find product by id - create method to get product by id - create schema to validate product id - create route to get produt by id

--- back/src/modules/products/controllers/product.controller.js
+++ back/src/modules/products/controllers/product.controller.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { ProductService } from '../services/product.service.js';
 import { validatorHandler } from '../../../middlewares/validatorHandler.js';
-import { createProductSchema } from '../schemas/product.schema.js';
+import {
+  createProductSchema,
+  findProductSchema,
+} from '../schemas/product.schema.js';
 
 export const productController = Router();
 const productService = new ProductService();
@@ -28,3 +31,17 @@ productController.get('/', async (req, res, next) => {
     next(error);
   }
 });
+
+productController.get(
+  '/:id',
+  validatorHandler(findProductSchema, 'params'),
+  async (req, res, next) => {
+    const id = req.params.id;
+    try {
+      const product = await productService.findById(id);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
