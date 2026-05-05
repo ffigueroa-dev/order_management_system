dd9a9d114e0258660af0db953d31f921b9d3288c
Facundo Figueroa Development <facundo.figueroa.dev@mail.com>
2026-05-05 16:07:12 -0300

    feat(login): jwt login - create env file - update .gitignore file - install axios and zustand - create axios config - save user and token -Create zustand auth store - create protected routes file

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router';
 
 import { router } from '@/routes/router';
 
+import { useAuthStore } from '@/features/auth/store/auth.store';
+
 const App = () => {
+  const loadSession = useAuthStore((state) => state.loadSession);
+  useEffect(() => {
+    loadSession();
+  }, []);
+
   return <RouterProvider router={router} />;
 };
 
